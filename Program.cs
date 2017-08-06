895676dce556e2d6a157162463e1c84a875f7a2e
harpreet97 <harrycheema97@yahoo.com>
2017-08-06 23:08:56 +1200

    task updated

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace week04task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Plese type a number");
+            string input = Console.ReadLine();
+            int output = 0;
+
+            if (int.TryParse(input, out output))
+            {
+                Console.WriteLine("Entered number is:" + output);
+                if (output > 12)
+                {
+
+                }
+                else
+                {
+                    for (int i = 1; i <= 12; i++)
+                    {
+                        int total=output*i;
+                        Console.WriteLine(string.Format("{0}x{1}={2}",output,i,total));
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please enter a valid number");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
